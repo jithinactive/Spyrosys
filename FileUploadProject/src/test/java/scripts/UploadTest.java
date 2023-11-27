2416098026067d7e875ff7a578c505b86afccabd
jithinactive <115936158+jithinactive@users.noreply.github.com>
2023-11-27 11:09:59 +0530

    Code changed

--- FileUploadProject/src/test/java/scripts/UploadTest.java
+++ FileUploadProject/src/test/java/scripts/UploadTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.*;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class UploadTest {
 
@@ -20,16 +20,23 @@ public class UploadTest {
         driver = new ChromeDriver();
         driver.get("https://www.ilovepdf.com/pdf_to_word");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
     @Test
-    public void fileLocate() throws AWTException {
+    public void fileLocate() throws Exception{
 
-        driver.findElement(By.cssSelector("#pickfiles"));
+        driver.findElement(By.cssSelector("#pickfiles")).click();
         fileUpload("C:\\Users\\admin2\\Desktop\\Jithin\\jiobill.pdf");
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.findElement(By.id("processTaskTextBtn")).click();
+        String text = driver.getTitle();
+        if(text.contains("Your PDF has been converted to an editable WORD document")){
+            System.out.println("File uploaded and converted");
+        }
+        else {
+            System.out.println("Task Failed");
+        }
     }
     public void fileUpload(String p) throws AWTException {
 
@@ -44,15 +51,6 @@ public class UploadTest {
         robot.keyRelease(KeyEvent.VK_V);
         robot.keyRelease(KeyEvent.VK_CONTROL);
         robot.keyPress(KeyEvent.VK_ENTER);
-
-        driver.findElement(By.cssSelector("#processTask")).click();
-        String text = driver.findElement(By.cssSelector("h1.title2")).getText();
-        if(text.equals("Your PDF has been converted to an editable WORD document")){
-            System.out.println("File uploaded and converted");
-        }
-        else {
-            System.out.println("Task Failed");
-        }
 
     }
 
